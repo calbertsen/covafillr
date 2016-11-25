d5ca0a1c9e4ed2ef68a59b9080c92a1a26f408de
Christoffer Moesgaard Albertsen <xpv919@alumni.ku.dk>
2016-11-25 13:39:01 +0100

    kde function

---
+++ R/kde.R
@@ -0,0 +1,55 @@
+
+##' Suggest bandwith for local polynomial regression
+##'
+##' The bandwith is suggested coordinate wise to be
+##' \deqn{0.9 \sqrt{5} \min\left(sd(x),\frac{IQR(x)}{1.349}\right) n ^{-0.2}} (p+1)
+##' where p is the polynomial degree used and n is the number of coordinate points.
+##' @param X A numeric matrix or vector of data coordinates
+##' @return a vector or scalar of suggested bandwiths
+##' @author Christoffer Moesgaard Albertsen
+##' @export
+suggestBandwith <- function(X, p){
+    d <- ifelse(is.matrix(X),ncol(X),1)
+    n <- ifelse(is.matrix(X),nrow(X),length(X))
+    if(d == 1){
+        bw <- 0.9 * sqrt(5) * min(sd(X), IQR(X) / 1.349) * n^(-0.2) * (max(p,0) + 1)
+    }else{
+        bw <- 0.9 * sqrt(5) * apply(X,2,function(x){min(sd(x),IQR(x)/1.349)})/n^(0.2) * (max(p,0) + 1)
+    }
+    return(bw)
+}
+
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title 
+##' @param X A numeric matrix or vector of data coordinates
+##' @param bw Bandwith used
+##' @param npred Number of coordinate wise equally spaced points at which the density is to be estimated. The numbers are repeated if the length is less than the dimension of the coordinates.
+##' @param from Coordinate wise lower bound of points at which the density is to be estimated. The numbers are repeated if the length is less than the dimension of the coordinates.
+##' @param to Coordinate wise upper bound of points at which the density is to be estimated. The numbers are repeated if the length is less than the dimension of the coordinates.
+##' @return a list of coordinates and corresponding density estimates
+##' @author Christoffer Moesgaard Albertsen
+##' @export
+kde <- function(X,bw = suggestBandwith(X,-1),npred = 100, from = min(X), to = max(X)){
+   
+    d <- ifelse(is.matrix(X),ncol(X),1)
+    n <- ifelse(is.matrix(X),nrow(X),length(X))
+    cf <- covafill(coord=X,obs=rep(1,n),p=-1L,h=bw)
+    I <- d * (d + 2) * gamma(d/2) / (4 * pi ^ (d / 2))
+
+    if(length(npred) < d)
+        npred <- rep(npred, length = d)
+    if(length(from) < d)
+        from <- rep(from, length = d)
+    if(length(to) < d)
+        to <- rep(to, length = d)
+
+    coords <- expand.grid(lapply(as.list(1:d),function(i)seq(from[i],to[i],length=npred[i])))
+    dens <- apply(coords,1,function(x)I*cf$predict(matrix(x,1)))
+
+    if(!is.null(colnames(X)))
+        colnames(coords) <- colnames(X)
+    
+    return(list(coord=coords,density=dens))
+}
